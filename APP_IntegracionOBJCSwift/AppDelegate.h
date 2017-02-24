233aa9c4142f39f04737ae00ae489386c77e3a98
cice <cice@aula8PovMacN08.local>
2017-02-24 19:23:28 +0100

    Initial Commit

---
+++ APP_IntegracionOBJCSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APP_IntegracionOBJCSwift
+//
+//  Created by cice on 24/2/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APP_IntegracionOBJCSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APP_IntegracionOBJCSwift
+//
+//  Created by cice on 24/2/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
